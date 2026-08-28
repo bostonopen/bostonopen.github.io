8bc54be1833271ce4e919243b76969c52b9889f5
Philip Durbin <philipdurbin@gmail.com>
2026-08-28 07:50:01 -0400

    Revert "switch map from OSM tiles to Carto Voyager" #26
    This reverts commit cacceef3bb10f3076a59112c3cc2a7474316edf7.

--- map.js
+++ map.js
@@ -1,9 +1,7 @@
 var map = L.map('map').setView([42.359068001401006, -71.09147396226346], 13);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 fetch('places.json')
