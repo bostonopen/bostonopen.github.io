80e909d0e7fec098f2f17c24fce0814982009eec
Philip Durbin <philipdurbin@gmail.com>
2026-09-23 22:00:04 -0400

    hide airport

--- map.js
+++ map.js
@@ -1,10 +1,18 @@
 var map = L.map('map').setView([42.359068001401006, -71.09147396226346], 13);
 
-L.maplibreGL({
+var vectorLayer = L.maplibreGL({
     style: 'https://tiles.openfreemap.org/styles/bright',
     attribution: '<a href="https://openfreemap.org">OpenFreeMap</a> '
         + '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+vectorLayer.getMaplibreMap().once('load', function () {
+    // Bright splits POI symbols by rank and transit type. The airport layer
+    // is separate from the poi source layer, so hide it as well.
+    ['poi_r20', 'poi_r7', 'poi_r1', 'poi_transit', 'airport'].forEach(function (layerId) {
+        vectorLayer.getMaplibreMap().setLayoutProperty(layerId, 'visibility', 'none');
+    });
+});
 
 fetch('places.json')
     .then(response => response.json())
